a3e65a12fc93c3d1b5196b9d075124cff0aeb546
Nathan Roark <nathanroark@protonmail.com>
2025-12-01 10:12:50 -0600

    idk yet

---
+++ src/2025/day-01/python/part_2.py
@@ -0,0 +1,47 @@
+class c:
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    ORANGE = "\033[93m"
+    BLUE = "\033[94m"
+    PINK = "\033[95m"
+    CYAN = "\033[96m"
+
+
+def fancy(value):
+    if value == 0:
+        value = f"{c.RED}{value}{c.ENDC}"
+    else:
+        value = f"{c.ORANGE}{value}{c.ENDC}"
+    return value
+
+
+def Part2(input):
+    password = 50
+    count = 0
+    lines = input.strip().split("\n")
+    for line in lines:
+        direction = line[0]
+        rotation = int(line[1:])
+        if direction == "L":
+            rotation *= -1
+            temp = (password + rotation) // 100
+            rotation %= 100
+
+        print("----------------")
+        print(f"p {password}")
+        print(f"r {rotation}")
+        print(f"t {temp}")
+        password += rotation
+        password %= 100
+        print(f"p {password}")
+        print("----------------")
+
+        # print(f"The dial is rotated {line} to point at {fancy(password)}.")
+
+        if password == 0:
+            count += 1
+
+    return count
