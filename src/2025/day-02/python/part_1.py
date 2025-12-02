8c525de5c6694e37b444449b5fe81d8bf27d744d
Nathan Roark <nathanroark@protonmail.com>
2025-12-02 07:49:40 -0600

    day2

--- src/2025/day-02/python/part_1.py
+++ src/2025/day-02/python/part_1.py
@@ -1,13 +1,9 @@
 def Part1(input):
-    password, count = 50, 0
-    for line in input.strip().splitlines():
-        direction, rotation = line[0], int(line[1:])
-        rotation = rotation if direction == "R" else -rotation
-
-        password = (password + rotation) % 100
-
-        # print(f"The dial is rotated {line} to point at {fancy(password)}.")
-
-        count += 1 if password == 0 else 0
-
-    return count
+    data = input.strip().split(",")
+    for d in data:
+        id = d.split("-")
+        first_id, last_id = id[0], id[1]
+        print("--------------")
+        print(f"{first_id}")
+        print(f"{last_id}")
+        print("--------------")
